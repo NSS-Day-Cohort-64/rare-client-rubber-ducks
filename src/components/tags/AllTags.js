44dae1be98ad0ef8c13020beaa04d7b2f541200f
Daniel Cicalese <dcik02@gmail.com>
2023-08-14 11:00:53 -0400

    minor changes

---
+++ src/components/tags/AllTags.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react"
+
+export const AllTags = () => {
+    const [tags, setTags] = useState([])
+    const [input, setInput] = useState("")
+
+    useEffect(
+        () => {
+        getTags()
+        },
+        []
+    )
+    
+    const getTags = () => {
+        fetch("http://localhost:8088/tags")
+            .then(response => response.json())
+            .then(data => setTags(data))
+        }
+
+    const addTag = (e) => {
+        e.preventDefault()
+        fetch("http://localhost:8088/tags", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({"label": input})
+    })
+        .then(response => response.json())
+        .then(result => {
+            setInput("")
+            getTags()
+        })
+    }
+
+    return (
+        <>
+            <h2>Tags</h2>
+            <div className="Tags-Content">
+            <article className="Tags">
+                {tags.slice().sort((a, b) => a.label.localeCompare(b.label)).map((tag) => (
+                    <section className="tag" key={`tag--${tag.id}`}>
+                        <ul>
+                            <li>{tag.label}</li>
+                        </ul>
+                        <div className="buttons">
+                            <button className="edit-button">
+                                Edit
+                            </button>
+                            <button className="delete-button">
+                                Delete
+                            </button>
+                        </div>
+                    </section>
+                ))}
+            </article>
+
+            <form>
+                <input
+                    value={input}
+                    placeholder="Insert Tag Name"
+                    onChange={(e) => setInput(e.target.value)}>
+                </input>
+                <button className="add-button button"
+                    onClick={(e) => addTag(e)}>
+                Create
+                </button>
+
+            </form>
+            </div>
+        </>
+    )
+}
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -4,19 +4,22 @@ import { Register } from "../components/auth/Register"
 import { Authorized } from "./Authorized"
 import { AllPostsPage } from "../components/posts/AllPostsPage"
 import { AllCategories } from "../components/categories/AllCategories"
+import { UserPostsPage } from "../components/userPosts/UserPostsPage"
+import { AllTags } from "../components/tags/AllTags"
+
 
 export const ApplicationViews = ({ token, setToken }) => {
   return <>
+    <h1>hello</h1>
     <Routes>
       <Route path="/login" element={<Login setToken={setToken} />}  />
       <Route path="/register" element={<Register setToken={setToken} />}  />
       <Route path="/posts" element={<AllPostsPage />}  />
       <Route path="/categories" element={<AllCategories />}  />
       <Route path="/tags" element={<AllTags />}  />
+      <Route path="/userPosts" element={<UserPostsPage />}  />
       <Route element={<Authorized token={token} />}/>
         {/* Add Routes here */}
-        
-      </Route>
     </Routes>
   </>
 }
