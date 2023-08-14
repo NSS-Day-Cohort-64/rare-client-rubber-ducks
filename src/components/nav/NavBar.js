4ad0ab68a59f7e1e74e83d040698e6709d4bb798
Daniel Cicalese <dcik02@gmail.com>
2023-08-14 12:36:26 -0400

    fixed nav

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -37,13 +37,10 @@ export const NavBar = ({ token, setToken }) => {
               <Link to="/posts" className="navbar-item">Posts</Link>
           
               <Link to="/categories" className="navbar-item">Manage Category</Link>
-<<<<<<< HEAD
               
               <Link to="/userPosts" className="navbar-item">My Posts</Link>
-=======
           
               <Link to="/tags" className="navbar-item">Tag Manager</Link>
->>>>>>> main
               </>
               :
               ""
@@ -67,12 +64,6 @@ export const NavBar = ({ token, setToken }) => {
                   <>
                     <Link to="/register" className="button is-link">Register</Link>
                     <Link to="/login" className="button is-outlined">Login</Link>
-<<<<<<< HEAD
-=======
-                    <Link to="/categories" className="navbar-item">Manage Category</Link>
-                    <Link to="/posts" className="navbar-item">Posts</Link>
-                    <Link to="/tags" className="button is-outlined">Tag Manager</Link>
->>>>>>> main
                   </>
               }
             </div>
